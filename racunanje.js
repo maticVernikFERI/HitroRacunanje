64d4f152bf604bee228b1cc773cbbf8e83cfa9b9
maticVernikFERI <matic.vernik1@student.um.si>
2024-01-27 23:04:27 +0100

    .

--- racunanje.js
+++ racunanje.js
@@ -1,37 +1,61 @@
-$(document).ready(function () {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    let racunanjeDo = urlParams.get("racunanjeDo");
-    let operacije = urlParams.getAll("operacije");
-    let tipIgre = urlParams.get("tipIgre");
-    let rezultat = napisiRacun(operacije, racunanjeDo);
-    let stPravilnih = 0;
-    let stNarobnih = 0;
+let countdown;
+let stPravilnih = 0;
+let stNarobnih = 0;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let racunanjeDo = urlParams.get("racunanjeDo");
+let operacije = urlParams.getAll("operacije");
+let tipIgre = urlParams.get("tipIgre");
+let rezultat;
 
+$(document).ready(function () {
+    startNew();
+    startcountdown();
     $(".num").click(beriRezultat);
     $("#oddaj").click(function () {
-        if(oddajRezultat(rezultat)) {
-            stPravilnih++;
-        } else {
-            stNarobnih++;
-        }
-        rezultat = napisiRacun(operacije, racunanjeDo);
+        oddajRezultat(rezultat);
+        startNew();
         $("#odgovor").html("");
     });
 });
 
+function startNew() {
+    if(stPravilnih + stNarobnih == 10){
+        setCookie("stPravilnih", stPravilnih, 1);
+        setCookie("stNarobnih", stNarobnih, 1);
+        window.open("rezultati.html", "_self");
+    }
+    rezultat = napisiRacun(operacije, racunanjeDo);
+
+}
+
+function startcountdown() {
+    let seconds = 10;
+    clearInterval(countdown);
+    countdown = setInterval(function () {
+        seconds--;
+        $("#timer").html(seconds + "s");
+        if (seconds == 0) {
+            clearInterval(countdown);
+            oddajRezultat(null);
+            startNew();
+        }
+    }, 1000);
+}
+
 function oddajRezultat(rezultat) {
     let napisanRezultat = $("#odgovor").html();
     if (rezultat == napisanRezultat) {
-        return true;
+        stPravilnih++;
     } else {
-        return false;
+        stNarobnih++;
     }
+    startcountdown(countdown);
+    console.log(stPravilnih, stNarobnih);
 }
 
 function beriRezultat() {
     let rezString = $("#odgovor").html();
-    console.log(rezString);
     rezString += $(this).html();
     $("#odgovor").html(rezString);
 }
@@ -44,7 +68,6 @@ function napisiRacun(operacije, racunanjeDo) {
     let stevki = dobiStevki(operacija, rezultat, racunanjeDo);
 
     let racun = stevki[0] + " " + operacija + " " + stevki[1] + " = ";
-    racun += rezultat;
 
     $("#enacba").html(racun);
 
@@ -78,8 +101,7 @@ function dobiStevkiZaPlus(rezultat) {
 function dobiStevkiZaMinus(rezultat, racunanjeDo) {
     racunanjeDo--;
     let stevka1 = getRandomInt(rezultat, racunanjeDo);
-    let stevka2 =  stevka1 - rezultat;
-    console.log(rezultat, stevka1, stevka2);
+    let stevka2 = stevka1 - rezultat;
     return [stevka1, stevka2];
 }
 function dobiStevkiZaKrat(rezultat, racunanjeDo) {
@@ -97,7 +119,6 @@ function dobiStevkiZaDeljeno(rezultat, racunanjeDo) {
         return [0, getRandomInt(0, racunanjeDo)];
     }
     let veckratniki = dobiVeckratnike(rezultat, racunanjeDo);
-    console.log(veckratniki);
     let stevka1 = veckratniki[getRandomInt(0, veckratniki.length - 1)];
     let stevka2 = stevka1 / rezultat;
     return [stevka1, stevka2];
@@ -122,9 +143,34 @@ function dobiDelitelje(stevilo) {
 function dobiVeckratnike(stevilo, meja) {
     let veckratniki = [];
     let i = 1;
-    while (stevilo*i < meja) {
+    while (stevilo * i < meja) {
         veckratniki.push(stevilo * i);
         i++;
     }
     return veckratniki;
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    value = value.toString();
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
